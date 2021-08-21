49f4a93287ed2900407b15dc619291b0495bb190
royenheart <72329961+royenheart@users.noreply.github.com>
2021-08-21 23:47:23 +0800

    添加服主信息查询以及php获取URL参数

--- databaseLinkNoSecret.php
+++ databaseLinkNoSecret.php
@@ -6,7 +6,7 @@
  * 
  * 此文件做数据库链接功能
  * 具体参数请以个人实际情况确定
- * 数据表中包含host，port，name字段
+ * 数据表中包含host，port，name，owner字段
  * 
  * 实际部署时请去掉文件前缀后'NoSecret'
  */
@@ -15,6 +15,7 @@
 $hosts = array();
 $ports = array();
 $names = array();
+$owners = array();
 
 /* 连接数据库 */
 $con = mysqli_connect('数据库地址','用户名','用户密码','使用的数据库');
@@ -30,6 +31,7 @@ while($row = mysqli_fetch_array($result)) {
     $hosts[count($hosts) + 1] = $row['host'];
     $ports[count($ports) + 1] = $row['port'];
     $names[count($names) + 1] = $row['name'];
+    $owners[count($owners) + 1] = $row['owner'];
 }
 
 mysqli_close($con);
