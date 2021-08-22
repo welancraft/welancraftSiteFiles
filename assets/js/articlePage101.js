883fd89d600196d7010f62f9f3ac47a14542951d
royenheart <72329961+royenheart@users.noreply.github.com>
2021-08-22 13:34:02 +0800

    修复元素间距错误问题

--- assets/js/articlePage101.js
+++ assets/js/articlePage101.js
@@ -1,10 +1,1 @@
-#articlePage101 h4,#articlePage101 h3,#articlePage101 p {
-    font-weight: bold;
-    text-align: center;
-    color: black;
-}
-
-#articlePage101 .content {
-    color: greenyellow;
-}
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-var addr = "https://mc.royenheart.com/resources/";
-var siteSources = addr+"siteSources/";
+var addr = "https://mc.royenheart.com/resources/"; // 资源地址
+var siteSources = addr+"siteSources/"; // 网页资源地址
 
 var illegal = ["100","101","102","103","104","105"]; // 增加参数检测
 
@@ -22,6 +22,8 @@ function getUrlString(name) {
 
 /* 图片栏图片随URL参数转换 */
 function picshowTextChange(value) {
+    /* 声明Image对象 */
+
     var picshowImage = new Image();
     
     /* 参数不符合规范，即value未在对应键值对中定义 */
@@ -30,27 +32,51 @@ function picshowTextChange(value) {
         value = "106"; // 跳转至出错界面
     }
 
-    /* 设置属性 */
+    /* 设置图片属性 */
+
     picshowImage.alt = "图片显示错误";
     picshowImage.id = "picshowPic";
     picshowImage.src = picshowSource[value][0];
     
-    /* 加入页面中 */
+    /* 图片插入页面 */
+
+    // 引用文档内对象
+    var header = document.getElementById("header");
     var picshow = document.getElementById("picshow");
     var picshowIntro = document.getElementById("picshowText");
+    var article = document.getElementById("article");
+    var footer = document.getElementById("footer");
     
-    var top = document.getElementById("header").clientHeight + "px"; // 设置图片栏与页眉上方距离
-    picshow.style.top = top;
+    // 获取宽度与高度
+    var valueOfSize = {
+        "width": {
+            "header": header.clientWidth,
+            "picshow": picshow.clientWidth,
+            "article": article.clientWidth,
+            "footer": footer.clientWidth
+        },
+        "height": {
+            "header": header.clientHeight,
+            "picshow": picshow.clientHeight,
+            "article": article.clientHeight,
+            "footer": footer.clientHeight
+        }
+    }
 
+    picshow.style.top = valueOfSize["height"]["header"] + "px"; // 设置图片栏与页眉距离
+
+    // 添加图片
     picshow.removeChild(document.getElementById("picshowPic"));
     picshow.appendChild(picshowImage);
 
     timer = setInterval(function() {
         if(picshowImage.complete) {
-            var height = document.getElementById("picshowPic").clientHeight + "px";
-            picshow.style.height = height; // 根据载入的图片设置图片栏高度
-            picshowIntro.innerHTML = picshowSource[value][1];
-            picshowIntro.style.top =  (document.getElementById("picshowPic").clientHeight-picshowIntro.clientHeight)/2+"px";
+            var picHeight = document.getElementById("picshowPic").clientHeight;
+            picshow.style.height = picHeight + "px"; // 根据载入的图片设置图片栏高度
+            picshowIntro.innerHTML = picshowSource[value][1]; // 更改信息
+            picshowIntro.style.top =  (picHeight-picshowIntro.clientHeight)/2+"px";
+            article.style.top = valueOfSize["height"]["header"] + "px";
+            footer.style.top = valueOfSize["height"]["header"] + "px"; // 由于是relative，因此相对于body位移页眉高度
             clearInterval(timer);
         }
     }, 100);
@@ -61,6 +87,7 @@ function load() {
     let urlPage = getUrlString("page"); // 获取page参数，str字符串形式
 
     /* 更改图片栏 */
+
     picshowTextChange(urlPage);
 }
 
