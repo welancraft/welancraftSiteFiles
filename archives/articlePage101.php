c8b9b29e4b9a2f2554b64cde70649854b86275d0
royenheart <72329961+royenheart@users.noreply.github.com>
2021-08-23 22:19:37 +0800

    添加细节与版权声明

--- archives/articlePage101.php
+++ archives/articlePage101.php
@@ -16,21 +16,22 @@
 
 <!-- 正文 获取服务器信息并输出 -->
 
+<!-- 由GamerNoTitle的模板修改而来，数据获取修改自MCNewsTools -->
+
 <div id=articlePage101>
 <?php $exitence = count($hosts); // 获取服务器个数 ?>
 <?php include dirname(dirname(__FILE__)) . '/serverDataDisplay/data.php'; ?>
-    <hr>
-    <h3>目前公开的服务器共有<font class=content><?php echo $exitence; ?></font>个</h3>
+    <h3>目前公开的服务器共有<label class=content><?php echo $exitence; ?></label>个</h3>
 <?php for ($id = 1; $id <= $exitence; $id++) : ?>
     <hr>
-    <h4>服务器地址：<font class=content><?php echo $names[$id]; ?></font></h4>
-    <p>服务器状态：<font class=content><?php echo $statuses[$id]; ?></font></p>
-    <p>服主：<font class=content><?php echo $owners[$id]; ?></font></p>
-    <p>原始服务器地址：<font class=content><?php echo $hosts[$id].":"."$ports[$id]"; ?></font></p>
-    <p>平台：<font class=content><?php echo $platforms[$id]; ?></font></p>
-    <p>游戏类型：<font class=content><?php echo $gametypes[$id]; ?></font></p>
-    <p>可容纳最大玩家数：<font class=content><?php echo $players_maxs[$id]; ?></font></p>
-    <h4>目前在线玩家 <font class=content><?php echo $players_onlines[$id]; ?></font>/<font class=content><?php echo $players_maxs[$id]; ?></font></h4>
+    <h4>服务器地址：<label class=content><?php echo $names[$id]; ?></label></h4>
+    <p>服务器状态：<label class=content><?php echo $statuses[$id]; ?></label></p>
+    <p>服主：<label class=content><?php echo $owners[$id]; ?></label></p>
+    <p>原始服务器地址：<label class=content><?php echo $hosts[$id].":"."$ports[$id]"; ?></label></p>
+    <p>平台：<label class=content><?php echo $platforms[$id]; ?></label></p>
+    <p>游戏类型：<label class=content><?php echo $gametypes[$id]; ?></label></p>
+    <p>可容纳最大玩家数：<label class=content><?php echo $players_maxs[$id]; ?></label></p>
+    <h4>目前在线玩家 <label class=content><?php echo $players_onlines[$id]; ?></label>/<label class=content><?php echo $players_maxs[$id]; ?></label></h4>
     <?php if (is_array($PlayersList[$id])) : ?>
         <?php foreach ($PlayersList[$id] as $Player) : ?>
             <?php if ($platforms[$id] == "MINECRAFT") : ?>
@@ -46,6 +47,5 @@
             <p>无玩家在线</p>       
         <?php endif; ?>
     <?php endif; ?>
-    <hr>
 <?php endfor; ?>
 </div>
--- index.php
+++ index.php
@@ -40,35 +40,36 @@ include_once __DIR__ . '/databaseLink.php';
 
     <!-- 正文 article -->
 
-    <!-- 由于服务端是php先执行编译，因此使用php判断 -->
-
     <div id=article>
-        <?php
-            include __DIR__.'/pageDeliver.php';
+        <br>
+        <div id=articlePage>
+            <?php
+            include __DIR__ . '/pageDeliver.php';
             $curl = getParams(); // $curl存储获得参数信息，并以键值对的形式存储
             if (!empty($curl)) {
                 if ($curl["page"] == "100") {
-                    include __DIR__.'/archives/articlePage100.php';
+                    include __DIR__ . '/archives/articlePage100.php';
                 } else if ($curl["page"] == "101") {
-                    include __DIR__.'/archives/articlePage101.php';
+                    include __DIR__ . '/archives/articlePage101.php';
                 } else if ($curl["page"] == "102") {
-                    include __DIR__.'/archives/articlePage102.php';
+                    include __DIR__ . '/archives/articlePage102.php';
                 } else if ($curl["page"] == "103") {
-                    include __DIR__.'/archives/articlePage103.php';
+                    include __DIR__ . '/archives/articlePage103.php';
                 } else if ($curl["page"] == "104") {
-                    include __DIR__.'/archives/articlePage104.php';
+                    include __DIR__ . '/archives/articlePage104.php';
                 } else if ($curl["page"] == "105") {
-                    include __DIR__.'/archives/articlePage105.php';
+                    include __DIR__ . '/archives/articlePage105.php';
                 } else {
-                    include __DIR__.'/archives/default.php';
+                    include __DIR__ . '/archives/default.php';
                 }
             } else {
-                include __DIR__.'/archives/articlePage100.php';
+                include __DIR__ . '/archives/articlePage100.php';
             }
-        ?>
-        
+            ?>
+        </div>
+        <br>
     </div>
-    
+
     <!-- 页脚 footer -->
 
     <div id=footer>
@@ -78,8 +79,7 @@ include_once __DIR__ . '/databaseLink.php';
         <p>以上内容以 <a href="https://github.com/royenheart/welancraftSiteFiles/blob/main/LICENSE">MPL-2.0</a> 协议提供</p>
         <p>内容已于 <a href="https://github.com/royenheart/welancraftSiteFiles">GITHUB</a> 上发布</p>
         <p>浙ICP备2020042582号</p>
-        <p><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=62012302000279"><img
-            src="/resources/beianIcon.png" />甘公网安备62012302000279号</a>
+        <p><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=62012302000279"><img src="/resources/beianIcon.png" />甘公网安备62012302000279号</a>
         </p>
         <br>
     </div>
