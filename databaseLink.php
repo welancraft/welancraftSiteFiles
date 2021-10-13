d79d10c0a9e77d0a64827b3ffcd47ba0c662e0f9
royenheart <72329961+royenheart@users.noreply.github.com>
2021-10-14 00:02:47 +0800

    分离数据库数据和数据库连接

--- databaseLink.php
+++ databaseLink.php
@@ -2,19 +2,26 @@
 
 /**
  * 网站数据库链接
- * @author RoyenHeart
  * 
- * 此文件做数据库链接功能
+ * 实现数据库链接功能
  * 具体参数请以个人实际情况确定
  * 数据表中包含host，port，name，owner字段
  * 
- * 实际部署时请去掉文件前缀后'NoSecret'
+ * 建议将resources文件夹单独放置并设置安全权限
+ * 
+ * @author RoyenHeart
  */
 
+/* 获取数据库数据文件 */
+include __DIR__ . '/resources/code/dbconnect.php'; // 根据实际存放位置修改
+
 /* 连接数据库 */
-$con = mysqli_connect('主机地址','用户名','用户密码','使用的数据库');
-if (!$con) {
-    die('Could not connect: ' . mysqli_error($con));
+try {
+    $con = mysqli_connect($dbServer,$dbUserName,$dbPassword,$dbName); 
+} catch (Exception $e) {
+    echo "Oops, something may happen to the server <br>";
+    echo "Exception " . $e.getCode() . ": " . $e.getMessage() . "<br>".
+    " in " . $e.getFile() . " on line " . $e.getLine() . "<br>";
 }
 
 /* 声明服务器数据存储数组 */
@@ -26,7 +33,13 @@ $subdomains = array();
 
 /* 获取服务器数据 */
 $sql="SELECT * FROM 数据表名称"; // 获取数据表指令
-$result = mysqli_query($con,$sql);
+try {
+    $result = mysqli_query($con,$sql);
+} catch (Exception $e) {
+    echo "Oops, something may happen to the server <br>";
+    echo "Exception " . $e.getCode() . ": " . $e.getMessage() . "<br>".
+    " in " . $e.getFile() . " on line " . $e.getLine() . "<br>";
+}
 
 while($row = mysqli_fetch_array($result)) {
     $hosts[count($hosts) + 1] = $row['host']; // 获取服务器地址
@@ -42,7 +55,13 @@ $contactList = array();
 /* 获取联系数据 */
 
 $sql="SELECT * FROM 数据表名称";
-$result = mysqli_query($con,$sql);
+try {
+    $result = mysqli_query($con,$sql);
+} catch (Exception $e) {
+    echo "Oops, something may happen to the server <br>";
+    echo "Exception " . $e.getCode() . ": " . $e.getMessage() . "<br>".
+    " in " . $e.getFile() . " on line " . $e.getLine() . "<br>";
+}
 
 while ($row = mysqli_fetch_array($result)) {
     $person = new Owner(
